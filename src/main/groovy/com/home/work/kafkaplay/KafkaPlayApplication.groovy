f181043127ba99d3dd07ea9dcf4f839727b6ff2b
PuneethKumarCk <punith.564@gmail.com>
2018-01-31 23:40:56 +0530

    initial project structure

---
+++ src/main/groovy/com/home/work/kafkaplay/KafkaPlayApplication.groovy
@@ -0,0 +1,12 @@
+package com.home.work.kafkaplay
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class KafkaPlayApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run KafkaPlayApplication, args
+	}
+}
